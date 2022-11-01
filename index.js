c8c3db8d9947a0109f2b32bf1da8ada3fbc4bdb0
Ray <rjmaestas1991@gmail.com>
2022-11-01 09:42:11 -0600

    commit tree 8a81d7cb45fad0009ac8863a0ede81e6638788da

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+    function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    }
+
+    function setBestCustomer(){
+        bestCustomer = 'not bob'
+    }
+
+    function overwriteBestCustomer(){
+        bestCustomer = 'maybe bob'
+    }
+const leastFavoriteCustmer = 'kevin';
+
+    function changeLeastFavoriteCustomer(){
+        leastFavoriteCustmer = 'adam';
+    }
